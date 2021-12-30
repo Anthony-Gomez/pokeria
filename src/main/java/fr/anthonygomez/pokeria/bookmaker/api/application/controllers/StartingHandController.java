a0f49fdfb9eb289be05a5419008f661a91bcd780
Anthony GOMEZ <anthony.gomez@akt.io>
2021-12-30 22:43:45 +0100

    add getCardFromInitialAndSymbol(String initialAndSymbol) method into service and add into controller

--- src/main/java/fr/anthonygomez/pokeria/bookmaker/api/application/controllers/StartingHandController.java
+++ src/main/java/fr/anthonygomez/pokeria/bookmaker/api/application/controllers/StartingHandController.java
@@ -1,7 +1,6 @@
 package fr.anthonygomez.pokeria.bookmaker.api.application.controllers;
 
-import fr.anthonygomez.pokeria.bookmaker.api.application.domains.card.SuitEnum;
-import fr.anthonygomez.pokeria.bookmaker.api.application.domains.card.ValueEnum;
+import fr.anthonygomez.pokeria.bookmaker.api.application.domains.card.Card;
 import fr.anthonygomez.pokeria.bookmaker.api.services.card.CardDecoderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,28 +8,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class StartingHandController {
 
     @Autowired
     private CardDecoderService cardDecoderService;
 
-    @GetMapping("/starting-hand/" +
-            "{starting-hand-value-symbol-1}-{starting-hand-suit-initial-1}:{starting-hand-value-symbol-2}-{starting-hand-suit-initial-2}")
-    public ResponseEntity<String> get(
-            @PathVariable("starting-hand-value-symbol-1") String symbolValueStartingHand1,
-            @PathVariable("starting-hand-suit-initial-1") String initialSuitStartingHand1,
-            @PathVariable("starting-hand-value-symbol-2") String symbolValueStartingHand2,
-            @PathVariable("starting-hand-suit-initial-2") String initialSuitStartingHand2) {
+    @GetMapping("/{starting-hand-1}:{starting-hand-2}")
+    public ResponseEntity<List<Card>> get(@PathVariable("starting-hand-1") String startingHand1,
+                                          @PathVariable("starting-hand-2") String startingHand2) {
 
-        ValueEnum valueEnumStartingHand1 = cardDecoderService.findValueEnumFromSymbol(symbolValueStartingHand1);
-        SuitEnum suiteEnumStartingHand1 = cardDecoderService.findSuitEnumFromInitial(initialSuitStartingHand1);
+        Card card1 = cardDecoderService.getCardFromInitialAndSymbol(startingHand1);
+        Card card2 = cardDecoderService.getCardFromInitialAndSymbol(startingHand2);
 
-        ValueEnum valueEnumStartingHand2 = cardDecoderService.findValueEnumFromSymbol(symbolValueStartingHand2);
-        SuitEnum suiteEnumStartingHand2 = cardDecoderService.findSuitEnumFromInitial(initialSuitStartingHand2);
-
-        return ResponseEntity.ok("Your starting hand is " + valueEnumStartingHand1 + suiteEnumStartingHand1 + "+" +
-                valueEnumStartingHand2 + suiteEnumStartingHand2);
+        return ResponseEntity.ok(Arrays.asList(card1, card2));
     }
 
 }
--- src/main/java/fr/anthonygomez/pokeria/bookmaker/api/services/card/CardDecoderService.java
+++ src/main/java/fr/anthonygomez/pokeria/bookmaker/api/services/card/CardDecoderService.java
@@ -1,5 +1,6 @@
 package fr.anthonygomez.pokeria.bookmaker.api.services.card;
 
+import fr.anthonygomez.pokeria.bookmaker.api.application.domains.card.Card;
 import fr.anthonygomez.pokeria.bookmaker.api.application.domains.card.SuitEnum;
 import fr.anthonygomez.pokeria.bookmaker.api.application.domains.card.ValueEnum;
 import org.springframework.stereotype.Service;
@@ -9,14 +10,22 @@ import java.util.Arrays;
 @Service
 public class CardDecoderService {
 
-    public SuitEnum findSuitEnumFromInitial(String initialToFind) {
+    public Card getCardFromInitialAndSymbol(String initialAndSymbol) {
+        String symbol = initialAndSymbol.substring(0, 1);
+        String initial = initialAndSymbol.substring(1, 2);
+        ValueEnum value = this.findValueEnumFromSymbol(symbol);
+        SuitEnum suit = this.findSuitEnumFromInitial(initial);
+        return new Card(value, suit);
+    }
+
+    private SuitEnum findSuitEnumFromInitial(String initialToFind) {
         return Arrays.stream(SuitEnum.values())
                 .filter(suiteEnum -> suiteEnum.getInitial().equals(initialToFind))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(initialToFind + " is not a suite initial"));
     }
 
-    public ValueEnum findValueEnumFromSymbol(String symbolToFind) {
+    private ValueEnum findValueEnumFromSymbol(String symbolToFind) {
         return Arrays.stream(ValueEnum.values())
                 .filter(valueEnum -> valueEnum.getSymbol().equals(symbolToFind))
                 .findFirst()
